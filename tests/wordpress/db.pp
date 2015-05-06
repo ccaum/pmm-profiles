3b0a116577ecbbd7bae09a50e7c7d8d194c8875a
Carl Caum <carl@carlcaum.com>
2015-05-06 11:25:04 -0700

    Do not manage DB user and password

--- tests/wordpress/db.pp
+++ tests/wordpress/db.pp
@@ -1,6 +1,4 @@
 class { '::profile::wordpress::db':
   app_hosts   => ['localhost','test.system.com'],
   db_name     => 'wordpress',
-  db_user     => 'wordpress',
-  db_password => 'wordpress',
 }
