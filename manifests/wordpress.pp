dc6c8c40901287db2b6dcceb300d6025cb558e72
Carl Caum <carl@carlcaum.com>
2015-08-25 19:50:55 +0000

    Add integrated wordpress profile

--- manifests/wordpress.pp
+++ manifests/wordpress.pp
@@ -1,7 +1,22 @@
-class profile::wordpress (
-  $db_name,
-  $db_user,
-  $db_password,
-) { 
+class profile::wordpress {
 
+  class { 'apache':
+    default_vhost => true,
+  }
+
+  class { 'apache::mod::php': }
+
+  class { 'wordpress':
+    require => [
+      Class['mysql::server'],
+      Class['apache'],
+      Class['apache::mod::php'],
+    ],
+  }
+
+  class { 'mysql::server': }
+
+  apache::vhost { 'wordpress':
+    docroot => '/opt/wordpress',
+  }
 }
