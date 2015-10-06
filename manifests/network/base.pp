c63d77fddfffb29eabf4f131ccd53a9663e0904e
Carl Caum <carl@carlcaum.com>
2015-10-05 21:46:10 -0700

    Add interface manifests

--- manifests/network/base.pp
+++ manifests/network/base.pp
@@ -1,6 +1,7 @@
 class profile::network::base {
   require ciscopuppet::install
   include profile::network::base::snmp
+  include profile::network::base::interfaces
 
   service { 'puppet':
     ensure => running,
--- manifests/network/interfaces.pp
+++ manifests/network/interfaces.pp
@@ -1,26 +1,11 @@
-class profile::network::cisco::interfaces {
-  cisco_interface { 'Ethernet1/1' :
-    shutdown            => true,
-    switchport_mode     => disabled,
-    description         => 'managed by puppet',
-    ipv4_address        => '192.168.55.55',
-    ipv4_netmask_length => 24,
-    vrf                 => 'test',
+class profile::network::base::interfaces(
+  $interfaces,
+) {
+
+  Cisco_interface {
+    shutdown        => false,
+    switchport_mode => disabled,
   }
 
-  cisco_interface { 'Ethernet1/2':
-    description     => 'default',
-    shutdown        => 'default',
-    access_vlan     => 'default',
-    switchport_mode => access,
-  }
-
-  cisco_interface { 'Ethernet1/3':
-    switchport_mode               => trunk,
-  }
-
-  cisco_interface { 'Vlan22':
-    svi_autostate  => false,
-    svi_management => true,
-  }
+  create_resources('cisco_interface', $interfaces)
 }
